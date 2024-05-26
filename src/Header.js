ed2ba471171279f7d5339b413ebf18818864adc0
pari4041 <166627409+pari4041@users.noreply.github.com>
2024-05-26 11:05:34 -0700

    edit1

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import StoreIcon from "@mui/icons-material/Store";
 import SearchIcon from "@mui/icons-material/Search";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
-
-
 
 function Header() {
   const [{ basket }, dispatch] = useStateValue();
@@ -15,7 +13,7 @@ function Header() {
       <Link to="/" style={{ textDecoration: "none" }}>
         <div className="header__logo">
           <StoreIcon className="header__logoImage" fontSize="large" />
-          <h2 className="header__logoTitle">Online-Shopping</h2>
+          <h2 className="header__logoTitle">Online-candele-Shop</h2>
         </div>
       </Link>
 
@@ -27,7 +25,7 @@ function Header() {
       <div className="header__nav">
         <Link to="/login" style={{ textDecoration: "none" }}>
           <div className="nav__item">
-            <span className="nav__itemLineOne">Hello Guest</span>
+            <span className="nav__itemLineOne">Hi Guest</span>
             <span className="nav__itemLineTwo">Sign In</span>
           </div>
         </Link>
